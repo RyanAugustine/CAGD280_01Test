e2e29ee272ae3e490f5dd9513399c2a4d70def56
Picklechin87 <raaugustine@csuchico.edu>
2023-10-31 13:24:31 -0700

    Fixed bug for jumping + new objects

--- GitTest/Assets/Scripts/PlayerController.cs
+++ GitTest/Assets/Scripts/PlayerController.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
     //implement player movement
 
     public float speed;
+    private Vector3 movePos;
 
     private void MovePlayer()
     {
         //moves the player
+    }
+
+    private void Jump()
+    {
+        //makes the player jump
     }
 }
